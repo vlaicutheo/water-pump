61eb5f41ce3aabf9f6feeff18b9c702d980ca25d
vlaicutheo <95164828+vlaicutheo@users.noreply.github.com>
2022-09-18 14:26:28 +0300

    changed to english

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let umiditate = 0
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(1000)
+    basic.pause(5000)
     pins.digitalWritePin(DigitalPin.P0, 0)
     basic.showIcon(IconNames.Yes)
 })
@@ -16,13 +16,13 @@ basic.forever(function () {
     basic.showNumber(umiditate)
     basic.pause(2000)
     if (umiditate > 600) {
-        basic.showString("Uscat")
+        basic.showString("DRY")
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(5000)
         pins.digitalWritePin(DigitalPin.P0, 0)
         basic.showIcon(IconNames.Yes)
     } else {
-        basic.showString("Umed")
+        basic.showString("WET")
     }
     basic.pause(30000)
 })
